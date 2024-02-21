1fe9abe1d3c6a2645a43eeb1910a7ba2522214db
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-02-21 18:49:52 -0300

    first commit

---
+++ Exchange Click/Exchange_ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_ClickApp.swift
+//  Exchange Click
+//
+//  Created by Sebastian Marquez Rojas on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Exchange_ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
