955cede5f65667bbde7ad1b5e8f788433bd32935
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-03-27 19:39:27 -0300

    login view created

--- Exchange Click/View/User/LoginView.swift
+++ Exchange Click/View/User/LoginView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var email = ""
+    @State private var password = ""
+    @State private var login = LoginViewModel()
+        var device = UIDevice.current.userInterfaceIdiom
+           var body: some View {
+               ZStack{
+                   (LinearGradient(gradient: Gradient(colors: [.cyan,.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                   ).ignoresSafeArea(.all)
+                   ZStack{
+                       (LinearGradient(gradient: Gradient(colors: [.purple,.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                       ).ignoresSafeArea(.all)
+                       VStack(alignment: .center, spacing: 20)
+                       {
+                           Text("Exchange Click").font(.largeTitle).foregroundStyle(.white).bold().frame(alignment: .leading)
+                           Spacer()
+                           VStack{
+                               TextField("Email", text: $email)
+                                   .border(Color.white)
+                                   .foregroundStyle(Color.black)
+                                   .textFieldStyle(.roundedBorder)
+                                   .keyboardType(.emailAddress)
+                                   .textInputAutocapitalization(.never)
+                                   .frame(width: device == .pad ? 400 : nil)
+                               SecureField("Password", text: $password)
+                                   .border(Color.white)
+                                   .foregroundStyle(Color.black)
+                                   .textFieldStyle(.roundedBorder)
+                                   .frame(width: device == .pad ? 400 : nil)
+                                   .padding(.bottom,20)
+                               
+                               Button(action:{
+                                   login.login()
+                               }){
+                                   Text("Entrar")
+                                       .font(.title)
+                                       .frame(width: 200)
+                                       .foregroundStyle(.white)
+                                       .padding(.vertical,10)
+                                       
+                               }.background(Capsule().stroke(Color.white))
+                                   
+                           }.padding(.all)
+                           
+                           
+                           
+                       Spacer()
+                       }
+                   }
+                   
+               }
+              
     }
 }
 
